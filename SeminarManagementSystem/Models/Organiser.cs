d48b848dd86adeb7541d808d22065bb819245139
Diogo Poletti <12001683@students.koi.edu.au>
2022-01-27 17:47:26 +1100

    Restored DB and implemented Organiser Controller and View

--- SeminarManagementSystem/Models/Organiser.cs
+++ SeminarManagementSystem/Models/Organiser.cs
@@ -1,12 +1,19 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
 namespace SeminarManagementSystem.Models
 {
     public class Organiser
     {
-        public int organiserID  {get; set;}
-        public string organiser_name { get; set; }
-        public DateTime organiser_DOB { get; set; }
+            public int organiserID { get; set; }
+            public string organiser_name { get; set; }
+            public DateTime organiser_DOB { get; set; }
 
 
+        
     }
+
 }
+
